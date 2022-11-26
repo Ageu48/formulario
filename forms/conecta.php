1958bf5abd1ffd803e6bb30d1e5ff27cc0cf824e
Ageu Franco <ageuolivera48@gmail.com>
2022-11-26 12:00:01 -0400

    edição dos dados de conexão

--- forms/conecta.php
+++ forms/conecta.php
@@ -1,13 +1,11 @@
 <?php
-define('DB_HOST','localhost');
-define('DB_USER','root');
+define('DB_HOST','sua host');
+define('DB_USER','usuario da host');
 define('DB_PASS','');
-define('DB_NAME','contatos');
+define('DB_NAME','nome do banco de dados');
 
 function db_conecta()
 {
     $PDO = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
     return $PDO;
 }
-
-?>
--- forms/contato.php
+++ forms/contato.php
@@ -1,14 +1,14 @@
 <?php
 require 'conecta.php';
 
-if(isset($_GET['salvar']) and $_GET['salvar'] == 'enviado'){
+if (isset($_GET['salvar']) and $_GET['salvar'] == 'enviado') {
 
     $nome = $_POST['nome'];
     $email = $_POST['email'];
     $telefone = $_POST['telefone'];
     $mensagem = $_POST['mensagem'];
 
-    $stmt = db_conecta()->prepare("INSERT INTO usuarios (nome, email, telefone, mensagem) VALUES (:nome, :email, :telefone, :mensagem)");
+    $stmt = db_conecta()->prepare("INSERT INTO nome da tabela do seu banco de dados (nome, email, telefone, mensagem) VALUES (:nome, :email, :telefone, :mensagem)");
 
     $stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
     $stmt->bindParam(':email', $email, PDO::PARAM_STR);
@@ -24,44 +24,40 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'vendor/autoload.php';
 
-if(isset($_GET['salvar']) and $_GET['salvar'] == 'enviado'){
+if (isset($_GET['salvar']) and $_GET['salvar'] == 'enviado') {
 
     $mail = new PHPMailer(true);
 
-try {
-     //credenciais SMTP
-                     
-    $mail->isSMTP();                                            
-    $mail->Host       = 'smtp.gmail.com';                     
-    $mail->SMTPAuth   = true;                                   
-    $mail->Username   = 'ageunubank@gmail.com';                     
-    $mail->Password   = 'igskfmzjhpcyzsvo';                               
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
-    $mail->Port       = 465;  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS                     
+    try {
+        //credenciais SMTP
 
-    //Recebedor
-    $mail->setFrom('ageunubank@gmail.com', '');
-    $mail->addAddress('ageunubank@gmail.com', ''); 
-    $mail->addReplyTo('ageunubank@gmail.com', '');
-  
-    $mail->isHTML(true);      
-    $mail->Subject = 'Novo Contato - ImporteFile';
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'seu email aqui';
+        $mail->Password   = 'chave smtp do gmail';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;  //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS                     
 
-    $body = "Novo interessado:<br>
-    Nome: ".$_POST['nome']."<br>
-    Email: ".$_POST['email']."<br>
-    Telefone: ".$_POST['telefone']."<br>
-    Mensagem:<br>".$_POST['mensagem'];
+        //Recebedor
+        $mail->setFrom('email que recebe', '');
+        $mail->addAddress('email que recebe', '');
+        $mail->addReplyTo('email que recebe', '');
 
-    $mail->Body    = $body;
-    $mail->AltBody = '';
+        $mail->isHTML(true);
+        $mail->Subject = 'Mensagem personalizada1';
 
-    $mail->send();
- 
+        $body = "mensagem personalizada2:<br>
+    Nome: " . $_POST['nome'] . "<br>
+    Email: " . $_POST['email'] . "<br>
+    Telefone: " . $_POST['telefone'] . "<br>
+    Mensagem:<br>" . $_POST['mensagem'];
+
+        $mail->Body    = $body;
+        $mail->AltBody = '';
+
+        $mail->send();
     } catch (Exception $e) {
         echo "<script>alert('Erro ao enviar o email!');</script>: {$mail->ErrorInfo}";
     }
 }
-
-
-?>
